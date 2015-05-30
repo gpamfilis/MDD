c78bf2c79c5e08a2e78c61f35c2a0e1443feeb3e
George Pamfilis <gpamfilis@gmail.com>
2015-05-30 23:50:19 +0300

    created some dirs

--- complete_missing_data_handle_for_a_day.py
+++ complete_missing_data_handle_for_a_day.py
@@ -9,21 +9,25 @@ if a whole day's data is missing that line should be deleted or filled with a Na
 import os
 #import pandas as pd
 
-list_of_stations_crete = ['aghiosnikolaos','alikianos','anogeia','askyfou','vrysses','heraclion','heraclionwest','heraclionport','ierapetra','lentas','metaxochori','moires','paleochora','plakias','pyrathi','rethymno','samaria','samariagorge','sitia','spili','sfakia','tzermiado','falasarna','finokalia','fourfouras','fragmapotamon','chania','chaniacenter']
+list_of_stations_crete = ['aghiosnikolaos','alikianos','anogeia','askyfou','vrysses',
+                          'heraclion','heraclionwest','heraclionport','ierapetra','lentas','metaxochori','moires',
+                          'paleochora', 'plakias','pyrathi','rethymno','samaria','samariagorge','sitia','spili',
+                          'sfakia','tzermiado','falasarna','finokalia','fourfouras',
+                          'fragmapotamon','chania','chaniacenter']
 number_of_stations = len(list_of_stations_crete)
 
 #list the contents of the first folder in our CURRENT directory hence the [0]
 
 def correct_and_rename_files(stations):
     for j in range(len(stations)):
-        path = os.path.join(os.getcwd(),list_of_stations_crete[j])        
+        path = os.path.join(os.getcwd(), list_of_stations_crete[j])
         files = os.listdir(os.getcwd() + '/' + list_of_stations_crete[j])
         for i in range(len(files)): 
-            f = open(os.path.join(path,files[i]))
+            f = open(os.path.join(path, files[i]))
             lines = f.readlines()
             f.close()
-            os.remove(os.path.join(path,files[i]))
-            newfile = open(os.path.join(path,files[i][:-23]+'-'+'data_with_no_empty_days.txt'),'w')
+            os.remove(os.path.join(path, files[i]))
+            newfile = open(os.path.join(path, files[i][:-23]+'-'+'data_with_no_empty_days.txt'),'w')
             for line in lines:
                 try:
                     if line:
--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 setup(
     name='MDD',
     version='0.1',
-    packages=['pandas', 'dateutil'],
+    packages=['pandas', 'dateutil', 'numpy'],
     url='https://github.com/gpamfilis/MDD',
     license='',
     author='George Pamfilis',
     author_email='gpamfilis@gmail.com',
-    description='Meteorological Data Downloader'
+    description='Meteorological Data Downloader', requires=['numpy']
 )
