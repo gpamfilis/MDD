19df6a052600bc7b8f928e9e616e8e868c2a608e
George Pamfilis <gpamfilis@gmail.com>
2015-05-30 19:04:47 +0300

    created some dirs

---
+++ calculations.py
@@ -0,0 +1,11 @@
+__author__ = 'George Pamfilis'
+__version__ = '1.0'
+__contact__ = 'gpamfilis@gmail.com'
+
+
+class EvapoTranspiration(object):
+    """
+    this class uses the pennman montif method for calculating ET0
+    """
+    def __init__(self):
+        pass
