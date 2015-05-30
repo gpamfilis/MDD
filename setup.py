2e1823a26abf80c7734982bfaff06bc58d79be39
George Pamfilis <gpamfilis@gmail.com>
2015-05-30 18:59:59 +0300

    created some dirs

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='MDD',
+    version='0.1',
+    packages=['pandas', 'dateutil'],
+    url='https://github.com/gpamfilis/MDD',
+    license='',
+    author='George Pamfilis',
+    author_email='gpamfilis@gmail.com',
+    description='Meteorological Data Downloader'
+)
